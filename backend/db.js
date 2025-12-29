c2a60268e6d4563d32f554dc938b2355a91d4f60
sanudageenaka <sgeenaka@gmail.com>
2025-12-29 14:58:03 +0530

    #23
    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,17 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.DB_HOST,          // gateway01.ap-southeast-1.prod.aws.tidbcloud.com
+  user: process.env.DB_USER,          // xxxx.root
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME,      // test
+  port: Number(process.env.DB_PORT),  // 4000 (VERY IMPORTANT)
+  ssl: {
+    rejectUnauthorized: true,         // REQUIRED for TiDB
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
